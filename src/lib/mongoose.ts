d316fb30290909c0548bdd9836107e8ce34e57f3
Fahad Minhas <hafizfahad099@gmail.com>
2026-05-16 01:23:29 +0500

    changed the mongo db connectio from static tomongo db atlas

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -1,13 +1,7 @@
 import mongoose from 'mongoose';
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/medvision_test'
-
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable');
-}
-
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
-  console.log("Connected with Mongo DB Successfuly!");
-  return mongoose.connect('mongodb://localhost:27017/medvision_ai');
+  const atlasUri = "mongodb+srv://fahadminhas:fahadminhas@cluster0.fpnw4kv.mongodb.net/medvision-ai?appName=Cluster0";
+  console.log("Connecting to MongoDB Atlas...");
+  return mongoose.connect(atlasUri);
 }
