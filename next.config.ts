e93f5e36e40ac91427b4d3233000efe31528b73b
Fahad Minhas <hafizfahad099@gmail.com>
2026-05-15 20:39:03 +0500

    Removed Some Errors from next default files added CSS for Floathing Chatbot Page in global.css

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'via.placeholder.com',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
